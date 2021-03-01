f01b4638a0a422785b684ce01be7073faa0b3e99
lax1dude <cgiacun@gmail.com>
2021-03-01 15:45:22 -0800

    added a map based crafting book

---
+++ src/main/java/net/lax1dude/lsd_plugin/TiHKALMapRenderer.java
@@ -0,0 +1,58 @@
+package net.lax1dude.lsd_plugin;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapPalette;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+
+public class TiHKALMapRenderer extends MapRenderer {
+
+	private int pageNumber = 0;
+	private int prevPageNumber = -1;
+	
+	private static final int maxPages = 5;
+	
+	private static final BufferedImage[] pageCache = new BufferedImage[maxPages];
+	
+	public void nextPage() {
+		++pageNumber;
+		if(pageNumber >= maxPages) {
+			pageNumber = maxPages - 1;
+		}
+	}
+	
+	public void previousPage() {
+		--pageNumber;
+		if(pageNumber < 0) {
+			pageNumber = 0;
+		}
+	}
+	
+	private static void loadPage(int p) {
+		if(pageCache[p] == null) {
+			try {
+				pageCache[p] = ImageIO.read(TiHKALMapRenderer.class.getResource("/assets/lsdplugin/tihkal/page"+p+".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void render(MapView arg0, MapCanvas arg1, Player arg2) {
+		if(prevPageNumber != pageNumber) {
+			loadPage(pageNumber);
+			if(pageCache[pageNumber] != null) {
+				arg1.drawImage(0, 0, pageCache[pageNumber]);
+			}
+			prevPageNumber = pageNumber;
+		}
+	}
+
+}
